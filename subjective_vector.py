c6ef2958ec24430c2da2deb732f798c19b510a2f
student-mayuko <67144745+student-mayuko@users.noreply.github.com>
2021-03-31 22:48:51 +0900

    Update subjective_vector.py

--- subjective_vector.py
+++ subjective_vector.py
@@ -80,6 +80,10 @@ class SGD:
     #shrink_rate呼び出し多すぎだからタイミング変えようね
     def fit(self, word_info, word_vec):
         #初期設定
+        seed_number = 0
+        torch.random.seed(seed_number)
+        torch.cuda.manual_seed_all(seed_number)
+        torch.backends.cudnn.deterministic = True        
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         for i in range(len(word_vec)):
             for j in range(len(word_vec[i])):
